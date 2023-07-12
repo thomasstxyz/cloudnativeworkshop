115a753214080c40ed1365b4825c2eae44ab2572
Thomas Stadler <thomas@thomasst.xyz>
2023-07-12 12:08:30 +0200

    feat: html and http port configurable via vars
    Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-import sys
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
+app_text = os.environ['APP_TEXT']
+app_port = os.environ['APP_PORT']
+
+
 @app.route('/')
 def index():
-    confText = sys.argv[1] if len(sys.argv) > 1 else 'No parameter provided'
-    html = f'<h1>{text}</h1>'
+    html = f'<h1>{app_text}</h1>'
     return html
 
 if __name__ == '__main__':
-    confPort = sys.argv[2] if len(sys.argv) > 2 else 'No parameter provided'
-    app.run(host='0.0.0.0', port=confPort)
+    app.run(host='0.0.0.0', port=app_port)
